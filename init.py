d315491211e44ff6c2fa79bc7ad7feb85640703e
jasonw52603 <minecrafter10k@gmail.com>
2020-02-12 16:22:13 -0500

    concatenate all team dataframes

--- init.py
+++ init.py
@@ -1,29 +1,38 @@
 import pandas as pd
 import numpy as np
-import openpyxl as xl 
+import openpyxl as xl
 import config as cfg
 import dataFrameFunctions as dfFunc
 
 
-#init code goes here
+# Init code goes here
 
-wb = xl.load_workbook(filename = '/home/jasonw/Desktop/pyro-pandas/samplesheet.xlsx')
-dict = wb.sheetnames
-print(dict[3])
+# Sheet names
+wb = xl.load_workbook(filename='/home/jasonw/Desktop/pyro-pandas/samplesheet0.xlsx')
+sheetnames = wb.sheetnames
+print(sheetnames[3])
 
-rawSheet = pd.ExcelFile('~/Desktop/pyro-pandas/samplesheet.xlsx')
-df0 = pd.read_excel(rawSheet, dict[3])
+# Dataframe
+rawSheet = pd.ExcelFile('~/Desktop/pyro-pandas/samplesheet0.xlsx')
+df0 = pd.read_excel(rawSheet, sheetnames[3])
 print(df0)
 
-df0 = dfFunc.resetColumnNames(df0)
-df0 = dfFunc.resetRowNames(df0)
+for sheet_num in range(len(sheetnames)):
+    df0 = pd.read_excel(rawSheet, sheetnames[sheet_num])
 
-for rowName in cfg.rowsToIgnore:
-    print(rowName)
-    df0 = dfFunc.deleteRow(df0, rowName)
+    df0 = dfFunc.reset_column_names(df0)
+    df0 = dfFunc.reset_row_names(df0)
 
+    for rowName in cfg.rowsToIgnore:
+        df0 = dfFunc.delete_row(df0, rowName)
 
-# df0 = df0.rename(columns={'dfdf': '1'})
-print(df0)
+    df0 = dfFunc.reset_row_names(df0)
+
+    for column in range(3):  # Removes the extra statistics from the scouting app
+        df0 = df0[df0.columns[:-1]]
+
+    # df0 = df0.rename(columns={'dfdf': '1'})
+    print('\n \n' + sheetnames[sheet_num] + ' final result:')
+    print(df0)
 
 
