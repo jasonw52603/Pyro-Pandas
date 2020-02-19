2f478d26688ac3dabd749171e7b5668d4667c3d7
jasonw52603 <minecrafter10k@gmail.com>
2020-02-19 15:37:21 -0500

    formatting updated

--- dataFrameFunctions.py
+++ dataFrameFunctions.py
@@ -1,16 +1,18 @@
-import pandas as pd
 
-def deleteRow(dataframe, rowIndex):
-    dataframe = dataframe.drop(index=[rowIndex])
+
+def delete_row(dataframe, row_index):
+    dataframe = dataframe.drop(index=[row_index])
     return dataframe
 
-def resetColumnNames(dataframe):
+
+def reset_column_names(dataframe):
     # Renames columns to integers starting from 0
     for column in range(len(dataframe.columns)):
         dataframe = dataframe.rename(columns={dataframe.columns[column]: column})
     return dataframe
 
-def resetRowNames(dataframe):
+
+def reset_row_names(dataframe):
     for row in range(len(dataframe.index)):
         dataframe = dataframe.rename(index={dataframe.index[row]: row})
     return dataframe
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import pandas as pd 
+import pandas as pd
 import dataFunctions as fn
 # import init as init
 
-data = {'col1':[1,2], 'col2':[True, False]}
+data = {'col1': [1, 2], 'col2': [True, False]}
 df = pd.DataFrame(data=data)
 print(df)
