a9224aaf4ba0f534fd7f490b834007c31f490fb8
jasonw52603 <minecrafter10k@gmail.com>
2020-02-26 20:55:33 -0500

    added delete_column function

--- dataFrameFunctions.py
+++ dataFrameFunctions.py
@@ -1,8 +1,11 @@
 
 
 def delete_row(dataframe, row_index):
-    dataframe = dataframe.drop(index=[row_index])
-    return dataframe
+    return dataframe.drop(index=row_index)
+
+
+def delete_column(dataframe, column_index):
+    return dataframe.drop(columns=column_index)
 
 
 def reset_column_names(dataframe):
@@ -16,3 +19,7 @@ def reset_row_names(dataframe):
     for row in range(len(dataframe.index)):
         dataframe = dataframe.rename(index={dataframe.index[row]: row})
     return dataframe
+
+
+def get_end_column(dataframe):
+    return dataframe.columns.values[-1]
